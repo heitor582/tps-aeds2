046f5b536b842910f77a44f453a60ede6301e75c
Heitor Vieira <heitorvieira.pvd@gmail.com>
2025-04-15 20:55:41 -0300

    i really dont know more what im doing

--- tp2/Show.c
+++ tp2/Show.c
@@ -5,8 +5,7 @@
 #include <time.h>
 
 #define MAX_LINE 2048
-#define MAX_SHOWS 1000
-
+#define MAX_SHOWS 2000
 typedef struct {
     char* show_id;
     char* type;
@@ -22,11 +21,44 @@ typedef struct {
     char** listed_in;
     int listed_in_size;
 } Show;
-
 Show* shows[MAX_SHOWS];
 int shows_count = 0;
+int mov = 0;
+int comp = 0;
+void swap(char** v, int i, int j){
+    char* temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+    mov+=3;
+}
+int compare(char* a, char* b){
+    comp++;
+    return strcmp(a,b);
+}
 
-char* strdup_strip(const char* src) {
+void heapSort(char** v, int size){
+    for(int i = 1; i<size; i++){
+        for(int j = i; j>0 && strcmp(v[j], v[(j-1)/2]) > 0; j=(j-1)/2){
+            swap(v, j, (j-1)/2);
+        }
+    }
+
+    for(int i = size-1; i>=0; i--){
+        swap(v, i, 0);
+        int j =0;
+        while(j<i && (j*2+1 < i || j*2+2 < i)){
+            int ji = j*2+2 < i && strcmp(v[j*2+2], v[j*2+1]) > 0 ? j*2+2 : j*2+1;
+            if(strcmp(v[j], v[ji]) < 0){
+                swap(v,j,ji);
+                j=ji;
+            } else {
+                j=i;
+            }
+        }
+    }
+}
+
+char* strdup_strip(char* src) {
     while (*src == ' ' || *src == '"') src++; 
     size_t len = strlen(src);
     while (len > 0 && (src[len - 1] == ' ' || src[len - 1] == '"')) len--;
@@ -36,11 +68,12 @@ char* strdup_strip(const char* src) {
     return dst;
 }
 
-char** split(const char* str, int* count) {
-    char* temp = strdup(str);
-    char* token = strtok(temp, ",");
+char** split(char* str, int* count) {
     char** result = NULL;
     int size = 0;
+
+    char* temp = strdup(str);
+    char* token = strtok(temp, ",");
 
     while (token != NULL) {
         result = (char**)realloc(result, sizeof(char*) * (size + 1));
@@ -95,41 +128,55 @@ Show* parse_show(char* line) {
         return NULL;
     }
 
-    s->show_id = fields[0];
-    s->type = fields[1];
-    s->title = fields[2];
-    s->director = fields[3];
-    s->cast = split(fields[4], &s->cast_size); // ordernar dps
-    s->country = fields[5];
-    s->date_added = strlen(fields[6]) > 0 ? fields[6] : NULL;
+    s->show_id = strlen(fields[0]) > 0 ? fields[0] : "NaN";
+    s->type = strlen(fields[1]) > 0 ? fields[1] : "NaN";
+    s->title = strlen(fields[2]) > 0 ? fields[2] : "NaN";
+    s->director = strlen(fields[3]) > 0 ? fields[3] : "NaN";
+    s->cast = split(fields[4], &s->cast_size);
+    if (s->cast_size > 0) {
+        heapSort(s->cast, s->cast_size);
+    }
+    s->country = strlen(fields[5]) > 0 ? fields[5] : "NaN";
+    s->date_added = strlen(fields[6]) > 0 ? fields[6] : "NaN";
     s->release_year = strlen(fields[7]) > 0 ? atoi(fields[7]) : -1;
-    s->range = fields[8];
-    s->duration = fields[9];
-    s->listed_in = split(fields[10], &s->listed_in_size); // ordernar dps
+    s->range = strlen(fields[8]) > 0 ? fields[8] : "NaN";
+    s->duration = strlen(fields[9]) > 0 ? fields[9] : "NaN";
+    s->listed_in = split(fields[10], &s->listed_in_size);
+    if (s->listed_in_size > 0) {
+        heapSort(s->listed_in, s->listed_in_size);
+    }
 
     return s;
 }
 
 void print_show(Show* s) {
-    printf("=> %s ## %s ## %s ## %s ## [", s->show_id, s->type, s->title, s->director);
-    for (int i = 0; i < s->cast_size; i++) {
-        printf("%s%s", s->cast[i], i < s->cast_size - 1 ? ", " : "");
+    printf("=> %s ## %s ## %s ## %s ## [", s->show_id, s->title, s->type, s->director);
+    if(s->cast_size > 0){
+        for (int i = 0; i < s->cast_size; i++) {
+            printf("%s%s", s->cast[i], i < s->cast_size - 1 ? ", " : "");
+        }
+    } else {
+        printf("%s", "NaN");
     }
-    printf("] ## %s ## %s ## %d ## %s ## %s ## [", s->country, s->date_added ? s->date_added : "NaN", s->release_year, s->range, s->duration);
-    for (int i = 0; i < s->listed_in_size; i++) {
-        printf("%s%s", s->listed_in[i], i < s->listed_in_size - 1 ? ", " : "");
+    printf("] ## %s ## %s ## %d ## %s ## %s ## [", s->country, s->date_added, s->release_year, s->range, s->duration);
+    if(s->listed_in_size > 0){
+        for (int i = 0; i < s->listed_in_size; i++) {
+            printf("%s%s", s->listed_in[i], i < s->listed_in_size - 1 ? ", " : "");
+        }
+    } else {
+        printf("%s", "NaN");
     }
-    printf("]\n");
+    printf("] ##\n");
 }
 
-Show* search_by_id(const char* id) {
+Show* search_by_id(char* id) {
     for (int i = 0; i < shows_count; i++) {
         if (strcmp(shows[i]->show_id, id) == 0) return shows[i];
     }
     return NULL;
 }
 
-void start_shows(const char* path) {
+void start_shows(char* path) {
     FILE* file = fopen(path, "r");
     if (!file) {
         perror("Erro ao abrir o arquivo");
@@ -137,31 +184,78 @@ void start_shows(const char* path) {
     }
 
     char line[MAX_LINE];
-    fgets(line, sizeof(line), file); 
-
-    while (fgets(line, sizeof(line), file)) {
-        line[strcspn(line, "\n")] = 0;
-        shows[shows_count++] = parse_show(line);
+    fscanf(file, " %[^\r\n]", line);
+    int i = 1;
+    while (fscanf(file, " %[^\r\n]", line) != EOF) {
+        Show* s = parse_show(line);
+        if (s != NULL) {
+            shows[shows_count++] = s;
+        } else {
+            printf("[ERRO] Parse falhou para: %s\n", line);
+        }
     }
 
     fclose(file);
 }
-
+Show* binarySearch(Show localShows[], int size, char* title){
+    int left = 0, right = size-1;
+    while(left <= right){
+        int mid = left + (right-left)/2;
+        int comp = compare(localShows[mid].title, title);
+        if(comp == 0) {
+            return &localShows[mid];
+        }else if(comp > 0){
+            right = mid-1;
+        } else{
+            left = mid + 1;
+        }
+    }
+    return NULL;
+}
+void printVerde(char* filename, clock_t inicio){
+    char fullFilename[200];
+    sprintf(fullFilename, "867936_%s.txt", filename);
+    FILE *matricula = fopen(fullFilename, "w");
+    clock_t final = clock();
+    double tempo = (double)(final - inicio) / CLOCKS_PER_SEC;
+    fprintf(matricula, "859230\t%fs\t%d", tempo, comp);
+    fclose(matricula);
+}
+int compareShowsByTitle(const void* a, const void* b) {
+    Show* s1 = (Show*)a;
+    Show* s2 = (Show*)b;
+    return compare(s1->title, s2->title);
+}
 int main() {
-    start_shows("/tmp/disneyplus.csv");
-
-    char input[64];
-    while (fgets(input, sizeof(input), stdin)) {
-        input[strcspn(input, "\n")] = 0;
-        if (strcmp(input, "FIM") == 0) break;
-
-        Show* s = search_by_id(input);
+    start_shows("./tmp/disneyplus.csv");
+    char text[1000];
+    scanf(" %[^\r\n]", text);
+    Show localShows[MAX_SHOWS];
+    int count = 0;
+    while (strcmp((char*)text, "FIM")) {
+        Show* s = search_by_id(text);
         if (s) {
-            print_show(s);
+            localShows[count++] = *s;
         } else {
             printf("x Show not found!\n");
         }
+        scanf(" %[^\r\n]", text);
     }
 
+    // qsort(localShows, count, sizeof(Show), compareShowsByTitle);
+    // comp--;
+    // scanf(" %[^\r\n]", text);
+    // while (strcmp((char*)text, "FIM")) {
+    //     Show* s = binarySearch(localShows, count, text);
+    //     if(s != NULL){
+    //         printf("SIM\n");
+    //     }else{
+    //         printf("NAO\n");
+    //     }
+    //     scanf(" %[^\r\n]", text);
+    // }
+    // printVerde("binaria", inicio);
+    
+    clock_t inicio = clock();
     return 0;
 }
--- tp2/Show.java
+++ tp2/Show.java
@@ -1,19 +1,24 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Locale;
+import java.util.List;
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 public class Show{
-    static SimpleDateFormat ddf = new SimpleDateFormat("dd-MM-yyyy");
+    static SimpleDateFormat ddf = new SimpleDateFormat("MMMM d, yyyy");
 
     public static final String FILE_PATH = "/tmp/disneyplus.csv";
     public static ArrayList<Show> shows = new ArrayList<Show>();
+    private static int mov = 0;
+    private static int comp = 0;
 
     private String show_id;
     private String type;
@@ -52,15 +57,164 @@ public class Show{
     public Show setRange(String range) {this.range = range;return this;}
     public Show setDuration(String duration) {this.duration = duration;return this;}
     public Show setListed_in(String[] listed_in) {this.listed_in = listed_in;return this;}
+    
+    public void heapSort(String[] v){
+        for(int i = 1; i < v.length; i++){
+            for(int j = i; j > 0 && v[j].trim().compareTo(v[(j-1)/2].trim()) > 0; j = (j-1)/2){
+                String temp = v[j];
+                v[j] = v[(j-1)/2];
+                v[(j-1)/2] = temp;
+            }
+        }
+        for(int i = v.length-1; i>=0;){
+            String temp = v[i];
+            v[i] = v[0];
+            v[0] = temp;
+            i--;
+            int j = 0;
+            while(j < i){
+                if(j*2+1 <= i || j*2+2 <= i){
+                    int ji = j*2+2 <= i && v[j*2+2].trim().compareTo(v[j*2+1].trim()) > 0?  j*2+2 : j*2+1;
+                    if(v[j].trim().compareTo(v[ji].trim()) < 0) {
+                        String temp2 = v[j];
+                        v[j] = v[ji];
+                        v[ji] = temp2;
+                        j=ji;
+                    } else {j=i;}
+                } else {
+                    j=i;
+                }
+            }
+        }
+    }
+    public static void heapSort(List<Show> v){
+        for(int i = 1; i < v.size(); i++){
+            for(int j = i; j > 0 && 
+            (compare(v.get(j).director, v.get((j-1)/2).director) > 0 
+            || (compare(v.get(j).director, v.get((j-1)/2).director) == 0 
+                && compare(v.get(j).title, v.get((j-1)/2).title) > 0)); j = (j-1)/2){
+                swap(v, j, (j-1)/2);
+            }
+        }
+        for(int i = v.size()-1; i>=0;i--){
+            swap(v, 0, i);
+            int j = 0;
+            while(j < i){
+                if(j*2+1 < i || j*2+2 < i){
+                    int ji = j*2+2 < i && 
+                    (compare(v.get(j*2+2).director, v.get(j*2+1).director) > 0 || 
+                    compare(v.get(j*2+2).director, v.get(j*2+1).director) == 0 && 
+                    compare(v.get(j*2+2).title, v.get(j*2+1).title) > 0) ?  j*2+2 : j*2+1;
+                    int result = compare(v.get(j).director,v.get(ji).director);
+                    if(result < 0 || (result==0 && compare(v.get(j).title,v.get(ji).title) < 0)) {
+                        swap(v, j, ji);
+                        j=ji;
+                    } else {j=i;}
+                } else {
+                    j=i;
+                }
+            }
+        }
+    }
+    public static void merge(List<Show> arr, int left, int mid, int right){
+        int nLength = mid+1-left;
+        int nRight = right-mid;
+        List<Show> L = new ArrayList<>(), R = new ArrayList<>();
+        for(int i = 0; i < nLength; i++){
+            L.add(arr.get(left+i));
+        }
+        for(int i = 0; i < nRight; i++){
+            R.add(arr.get(mid+1+i));
+        }
+        for(int i = left, r = 0, l=0; i<=right; i++){
+            Show show = null;
+            if (l >= L.size()) {
+                show = R.get(r++);
+            } else if (r >= R.size()) {
+                show = L.get(l++);
+            } else {
+                int result = compareWithCase(L.get(l).duration, R.get(r).duration);
+                if(result < 0) {
+                    show = L.get(l);
+                    l++;
+                } else if(result == 0) {
+                    if(compare(L.get(l).title, R.get(r).title) < 0){
+                        show = L.get(l);
+                        l++;
+                    } else {
+                        show = R.get(r);
+                        r++;
+                    }
+                } else {
+                    show = R.get(r);
+                    r++;
+                }
+            }
+            mov++;
+            arr.set(i, show);
+        }
+    }
+    public static void mergeSort(List<Show> arr, int left, int right) {
+        if(left>=right){
+            return;
+        }
+        int mid = left+(right-left)/2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid+1, right);
+        merge(arr,left,mid,right);
+    }
+    public static void selectionSort(List<Show> shows){
+        for(int i = 0; i<shows.size()-1; i++){
+            int min = i;
+            for(int j = i+1; j<shows.size(); j++){
+                comp++;
+                if(compare(shows.get(min).getTitle(), shows.get(j).getTitle()) > 0){
+                    min = j;
+                }
+            }
+            swap(shows, i, min);
+        }
+    }
+    public static void insertionSort(List<Show> arr) {
+        for (int i = 1; i < arr.size(); i++) {
+            Show current = arr.get(i);
+            int j = i - 1;
+            
+            while (j >= 0 && (compare(arr.get(j).type, current.type) > 0 || 
+                              (compare(arr.get(j).type, current.type) == 0 && compare(arr.get(j).title, current.title) > 0))) {
+                arr.set(j + 1, arr.get(j));
+                j--;
+            }
+    
+            arr.set(j + 1, current);
+        }
+    }
+    public static void verdeSort(List<Show> list, long inicio, String filename, Consumer<List<Show>> sort) throws FileNotFoundException{
+        PrintWriter log = new PrintWriter("867936_"+filename+".txt");
 
+        sort.accept(list);
+        for(Show item : list){
+             item.print();
+        }
+        long fimTempo = System.currentTimeMillis();
+        log.println("867936\t" + comp + "\t" + mov + "\t" + (fimTempo - inicio));
+        log.close();
+    }
     public static Show searchById(String id, ArrayList<Show> shows) {
         for(int i = 0; i < shows.size(); i++) {
-
             if(shows.get(i).getShow_id().equals(id)) return shows.get(i);
         }
         return null;
     }
-
+    public static Show searchByTitle(String title, ArrayList<Show> shows) {
+        for(int i = 0; i < shows.size(); i++) {
+            comp++;
+            if(shows.get(i).getTitle().equals(title)) {
+                return shows.get(i);
+            };
+        }
+        return null;
+    }
     public void read(String line) {
         try {
             ArrayList<String> parts = new ArrayList<>();
@@ -84,14 +238,15 @@ public class Show{
             this.director = !parts.get(3).isEmpty() ? parts.get(3) : "NaN";
 
             this.cast = !parts.get(4).isEmpty()
-                    ? parts.get(4).split(",\\s*")
+                    ? parts.get(4).split(",\s*")
                     : new String[]{"NaN"};
+            this.heapSort(this.cast);
 
             this.country = !parts.get(5).isEmpty() ? parts.get(5) : "NaN";
 
             if (!parts.get(6).isEmpty()) {
                 try {
-                    this.date_added = new SimpleDateFormat("MMMM d, yyyy", Locale.ENGLISH).parse(parts.get(6));
+                    this.date_added = ddf.parse(parts.get(6));
                 } catch (ParseException e) {
                     this.date_added = null;
                 }
@@ -107,26 +262,50 @@ public class Show{
             this.duration = !parts.get(9).isEmpty() ? parts.get(9) : "NaN";
 
             this.listed_in = !parts.get(10).isEmpty()
-                    ? parts.get(10).split(",\\s*")
+                    ? parts.get(10).split(",\s*")
                     : new String[]{"NaN"};
+            this.heapSort(this.listed_in);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
     public void print() {
-        System.out.print("=> " + show_id + " ## " + type + " ## " + title + " ## " + director + " ## [");
+        System.out.print("=> " + show_id + " ## " + title + " ## " + type + " ## " + director + " ## [");
         System.out.print(String.join(", ", cast));
         System.out.print("] ## " + country + " ## ");
 
         System.out.print(date_added != null ? ddf.format(date_added) : "NaN");
         System.out.print(" ## " + release_year + " ## " + range + " ## " + duration + " ## [");
         System.out.print(String.join(", ", listed_in));
-        System.out.println("]");
+        System.out.println("] ##");
     }
-
-
+    public static void swap(List<Show> shows, int i, int j){
+        Show show = shows.get(i);
+        shows.set(i, shows.get(j));
+        shows.set(j, show);
+        mov+=3;
+    }
+    public static int compare(String a, String b){
+        comp++;
+        return a.trim().compareToIgnoreCase(b.trim());
+    }
+    public static int compareWithCase(String a, String b){
+        comp++;
+        return a.trim().compareTo(b.trim());
+    }
+    public static int compare(int a, int b){
+        comp++;
+        int result = 0;
+        if(a == b){
+            result = 0;
+        } else if(a > b){
+            result = 1;
+        } else {
+            result = -1;
+        }
+        return result;
+    }
     public static void startShows() {
         try {
             FileInputStream fstream = new FileInputStream(FILE_PATH);
@@ -145,24 +324,39 @@ public class Show{
         catch(IOException e) { e.printStackTrace(); }
     }
 
-    public static void main(String[] args) {
-        startShows();
-       Scanner inScanner = new Scanner(System.in);
+    public static void main(String[] args) throws FileNotFoundException {
+       startShows();
+       Scanner sc = new Scanner(System.in);
 
-       Show show = new Show();
-       String line = inScanner.nextLine();
+       ArrayList<Show> list = new ArrayList<>();
+       String line = sc.nextLine();
 
        while(!line.equals("FIM")) {
+         Show show = searchById(line, shows);
 
-           show = searchById(line, shows);
-
-           if(show != null) show.print();
+           if(show != null) list.add(show);
            else System.out.println("x Show not found!");
 
-           line = inScanner.nextLine();
+           line = sc.nextLine();
        }
 
-       inScanner.close();
+    //    line = sc.nextLine();
+       
+    //    while(!line.equals("FIM")){
+    //     Show show = searchByTitle(line, list);
+    //     if(show != null) System.out.println("SIM");
+    //        else System.out.println("NAO");
+
+    //        line = sc.nextLine();
+    //    }
+
+       sc.close();
+       long inicio = System.currentTimeMillis();
+       //verdeSort(list, inicio, "selecao", lista -> selectionSort(lista));
+       //verdeSort(list, inicio, "heapsort", lista -> heapSort(lista));
+       //verdeSort(list, inicio, "countingsort", lista -> mergeSort(lista,0,lista.size()-1));
+       verdeSort(list, inicio, "insercao", lista -> insertionSort(lista));
+    //    log.println("867936\t" + (fimTempo - inicio) + "\t" + comp); //sequencial
     }
 
 }
